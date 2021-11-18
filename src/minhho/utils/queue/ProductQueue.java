6fe4c24d9a36b125ea29149f5eec18f3b6ac3ab5
MinhHo <61931543+minhho89@users.noreply.github.com>
2021-11-18 08:06:18 +0700

    Feature 10: Implementing queue

--- src/minhho/utils/queue/ProductQueue.java
+++ src/minhho/utils/queue/ProductQueue.java
@@ -18,4 +18,25 @@ public class ProductQueue {
         products = new Product[this.capacity];
     }
 
+    boolean isFull()
+    {
+        return (this.size == this.capacity);
+    }
+
+    // Queue is empty when size is 0
+    boolean isEmpty()
+    {
+        return (this.size == 0);
+    }
+
+    public void enqueue(Product product) {
+        if (isFull()) {
+            return;
+        }
+        this.rear = (this.rear + 1)
+                % this.capacity;
+        this.products[this.rear] = product;
+        this.size = this.size + 1;
+    }
+
 }
